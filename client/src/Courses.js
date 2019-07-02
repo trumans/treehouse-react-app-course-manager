cc2c51f12d1ed01bbf10e5249508ef1c6564e57a
Truman Smith <trumanwsmith@gmail.com>
2019-07-02 14:56:16 -0700

    Adds New Course tile to Courses component

--- client/src/Courses.js
+++ client/src/Courses.js
@@ -17,16 +17,18 @@ class Courses extends Component {
   }
 
   render() {
+  	let tiles = null
   	console.log("in Courses. state is ", this.state);
   	if (this.state.courses !== undefined) {
-  	  return (
-  	    <div class='bounds'>
-          { this.state.courses.map((course) => <CourseTile title={course.title} courseId={course.id} /> ) }
-  	    </div>
-  	    )
-  	} else {
-  		return null
-  	}
+  	  tiles = this.state.courses.map((course) => <CourseTile title={course.title} courseId={course.id} /> )
+    }
+
+  	return (
+      <div class='bounds'>
+        { tiles }
+        <NewCourseTile />
+      </div>
+  	)
   }
 }
 
@@ -40,6 +42,24 @@ class CourseTile extends Component {
         <a class="course--module course--link" href={link}>
           <h4 class="course--label">Course</h4>
           <h3 class="course--title">{this.props.title}</h3>
+        </a>
+      </div>
+  	)
+  }
+}
+
+class NewCourseTile extends Component {
+  
+  render() {
+  	return (
+      <div class="grid-33">
+        <a class="course--module course--add--module" href="courses/create">
+          <h3 class="course--add--title">{this.props.title}
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 13 13" class="add">
+                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+            </svg>
+            "New Course"
+          </h3>
         </a>
       </div>
   	)
